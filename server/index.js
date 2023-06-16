600f82613a3d10dab6afc542fffb015eabaac5b2
Abigael Kirwa <abigael.kirwa@strathmore.edu>
2023-06-16 05:21:16 +0300

    added form and correspoinding code on client

--- server/index.js
+++ server/index.js
@@ -2,3 +2,13 @@
 
 // production - node js server + static react files
 
+const express = require('express')
+const app = express()
+
+app.get('/hello', (req, res) =>{
+    res.send('hello world')
+})
+
+app.listen(1337, () =>{
+    console.log("Server started on 1337")
+})
